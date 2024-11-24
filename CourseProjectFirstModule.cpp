2cbe2f5a4227f298563aff1ead39d38a82b5d9f4
Alexei Petrov <petrovAlekse95@mail.ru>
2024-11-24 03:37:40 +0300

    I can't pass verification

--- CourseProjectFirstModule.cpp
+++ CourseProjectFirstModule.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
 #include <fstream>
-
+#include "windows.h"
 
 int** readingFile(int& row, int& column); // Reading a file. Returns an array of the world, the element of the array is 1 if there is life there and 0 if there is no
 void readingTwoDimensionalArray(int** array, int row, int column); // Reading an array
 void deletingTwoDimensionalArray(int** array, int row, int column); // Deleting an array
-void StateWorld(int** const array, int row, int column, int generation, int aliveCells); // Отрисовка состояния мира
-bool EndGameLife(int** const array, int** const prevArray, int row, int column, int generation, int aliveCells); //Конец игры, возвращает true если игра заончена и false если нет
-int** creatingTwoDimensionalArray(int row, int column); // Создание двухмерного массива
-void copyingStateWorld(int** const array, int** prevArray, int row, int column); // Копирование состояния мира, первый аргумент хранит рельное значение мира, второй аргумент копия 
-int sumAdjacentElements(int** prevArray, int row, int column, int currentRow, int currentColumn); // возвращает сумму соседних элементов в массиве
-void birthLife(int** array, int** prevArray, int row, int column); // В первом аргументе на основе второго аргумента создает "новую жизнь"
-void deathLife(int** array, int** prevArray, int row, int column); //  В первом аргументе на основе второго аргумента "уничтожает жизнь"
-
+void StateWorld(int** const array, int row, int column, int generation, int aliveCells); // Rendering the state of the world
+bool EndGameLife(int** const array, int** const prevArray, int row, int column, int generation); // End of game, returns true if the game is over and false if not
+int** creatingTwoDimensionalArray(int row, int column); // Creating a 2D Array
+void copyingStateWorld(int** const array, int** prevArray, int row, int column); // Copying the state of the world, the first argument stores the real value of the world, the second argument is a copy
+int sumAdjacentElements(int** prevArray, int row, int column, int currentRow, int currentColumn); // returns the sum of adjacent elements in an array
+void birthLife(int** array, int** prevArray, int row, int column); // In the first argument, based on the second argument, creates a "new life"
+void deathLife(int** array, int** prevArray, int row, int column); //  In the first argument based on the second argument "destroys life"
+int CalculationNumberLivingCells(int** const array, int row, int column); // Calculates the number of living cells
 
 int main()
 {
@@ -27,51 +27,20 @@ int main()
 
     while (true) {
 
-        if (EndGameLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column)) {
+        aliveCells = CalculationNumberLivingCells(arrayOfTheUniverse, row, column);
+
+        copyingStateWorld(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
+        StateWorld(arrayOfTheUniverse, row, column, generation, aliveCells);
+        birthLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
+        deathLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
+        generation++;
+
+        if (EndGameLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column, generation)) {
             break;
         };
 
-        copyingStateWorld(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-        birthLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-        deathLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-        StateWorld(arrayOfTheUniverse, row, column, generation, aliveCells);
-        generation++;
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-  /*  copyingStateWorld(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-    birthLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-    deathLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-    StateWorld(arrayOfTheUniverse, row, column, generation, aliveCells);*/
-    
-
-
-
-
-
-
-   /* StateWorld(arrayOfTheUniverse, row, column, generation, aliveCells);
-
-    copyingStateWorld(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column);
-
-    if (EndGameLife(arrayOfTheUniverse, prevArrayOfTheUniverse, row, column)) {
-        std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-    };*/
-
-
-
-
-
+        Sleep(1000);
+    };
 
 
     deletingTwoDimensionalArray(arrayOfTheUniverse, row, column);
@@ -161,31 +130,34 @@ void StateWorld(int** const array, int row, int column, int generation, int aliv
 }
 //
 
-bool EndGameLife(int** const array, int** const prevArray, int row, int column) {
+bool EndGameLife(int** const array, int** const prevArray, int row, int column, int generation) {
     bool endGameLife{ true };
-    for (int i{ 0 }; i < row; i++) {
-        for (int j{ 0 }; j < column; j++) {
-            array[i][j] == prevArray[i][j] ? (endGameLife = endGameLife && true) : (endGameLife = endGameLife && false);
-        };
-    };
-
-    if (endGameLife) {
-        std::cout << "The world has stagnated. Game over!" << std::endl;
-        return true;
-    }
-
-    endGameLife = true;
+    int aliveCells{ 0 };
+    aliveCells = CalculationNumberLivingCells(array, row, column);
 
     for (int i{ 0 }; i < row; i++) {
         for (int j{ 0 }; j < column; j++) {
             array[i][j] == 0 ? (endGameLife = endGameLife && true) : (endGameLife = endGameLife && false);
         };
     };
-
     if (endGameLife) {
+        StateWorld(array, row, column, generation++, aliveCells);
         std::cout << "All cells are dead.Game over!" << std::endl;
         return true;
     };
+
+
+    endGameLife = true;
+     for (int i{ 0 }; i < row; i++) {
+        for (int j{ 0 }; j < column; j++) {
+            array[i][j] == prevArray[i][j] ? (endGameLife = endGameLife && true) : (endGameLife = endGameLife && false);
+        };
+    };
+    if (endGameLife) {
+        StateWorld(array, row, column, generation++, aliveCells);
+        std::cout << "The world has stagnated. Game over!" << std::endl;
+        return true;
+    }
 }
 //
 
@@ -211,8 +183,8 @@ int sumAdjacentElements(int** prevArray, int row, int column, int currentRow, in
     int counter{ 0 };
     for (int i{ -1 }; i <= 1; i++) {
         for (int j{ -1 }; j <= 1; j++) {
-            if ( ( (currentRow + i) < 0 ) || ( (currentRow + i) >= (row - 1) ) ) continue;
-            if (((currentColumn + j) < 0) || ((currentColumn + j) >= (column - 1))) continue;
+            if ( ( (currentRow + i) < 0 ) || ( (currentRow + i) > (row - 1) ) ) continue;
+            if (((currentColumn + j) < 0) || ((currentColumn + j) > (column - 1))) continue;
             if (((currentRow + i) == currentRow) && ((currentColumn + j) == currentColumn)) continue;
 
             counter += prevArray[currentRow + i][currentColumn + j];
@@ -246,5 +218,17 @@ void deathLife(int** array, int** prevArray, int row, int column) {
             };
         }
     }
+}
+//
+
+int CalculationNumberLivingCells(int** const array, int row, int column) {
+    int counter{ 0 };
+    for (int i{ 0 }; i < row; i++) {
+        for (int j{ 0 }; j < column; j++) {
+            counter += array[i][j];
+        };
+    };
+
+    return counter;
 }
 //
